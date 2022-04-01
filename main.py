adaec804beb2f3e3aa038f451284afcffb070094
oneut <o.l.c.neut@student.tudelft.nl>
2022-04-01 12:41:50 +0200

    Added removal functionality

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from bw4t.BW4TWorld import BW4TWorld
 from bw4t.statistics import Statistics
 from agents1.BW4TBaselineAgent import BaseLineAgent
-<<<<<<< Updated upstream
-=======
-from agents1.BW4TStrongAgent import StrongAgent
-from agents1.BW4TLyingAgent import LyingAgent
+
 from agents1.BetterAgent import BetterAgent
 from agents1.ColorblindAgent import ColorblindAgent
->>>>>>> Stashed changes
+
 from agents1.BW4THuman import Human
 
 
@@ -18,24 +15,17 @@ press the start button in god mode to start the session.
 
 if __name__ == "__main__":
     agents = [
-<<<<<<< Updated upstream
-        {'name':'agent1', 'botclass':BaseLineAgent, 'settings':{'slowdown':10}},
-        {'name':'agent2', 'botclass':BaseLineAgent, 'settings':{}},
-        {'name':'human', 'botclass':Human, 'settings':{}}
-=======
         # {'name': 'strongagent', 'botclass': StrongAgent, 'settings': {}},
         # {'name': 'lyingagent', 'botclass': LyingAgent, 'settings': {}},
         # {'name': 'colorblindagent', 'botclass': ColorBlindAgent, 'settings': {}},
         {'name': 'betteragent', 'botclass': BetterAgent, 'settings': {}},
         {'name': 'colorblindagent', 'botclass': ColorblindAgent, 'settings': {}},
-
         # {'name':'agent1', 'botclass':BaseLineAgent, 'settings':{'slowdown':10}},
         # {'name':'agent2', 'botclass':BaseLineAgent, 'settings':{'slowdown':10}},
         # {'name': 'lyingAgent1', 'botclass': LyingAgent, 'settings': {}},
         # {'name': 'ourAgent', 'botclass': Agent, 'settings': {}},
         #
         # {'name':'human', 'botclass':Human, 'settings':{}}
->>>>>>> Stashed changes
         ]
 
     print("Started world...")
